8761572ce085a99f65dd4b48251badc4ee5a8b0a
André Sugai <guitarfreaks@gmail.com>
2026-01-12 19:04:59 -0300

    atualização de recursos e décadas

---
+++ extract_artists_1984.py
@@ -0,0 +1,19 @@
+import json
+
+def extract_artists(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    artists = set()
+    for video in data:
+        if 'artist_name' in video:
+            artists.add(video['artist_name'])
+        elif 'artist' in video:
+            artists.add(video['artist'])
+
+    return sorted([str(a) for a in artists])
+
+if __name__ == "__main__":
+    artists = extract_artists('data/1984.json')
+    for artist in artists:
+        print(artist)
